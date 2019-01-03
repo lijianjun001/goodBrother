536d16cf33ce34b68badb7ff80553e45c9ebf53a
lijianjun <1455404626@qq.com>
2019-01-03 17:02:09 +0800

    修改了RecycleView相关类

--- app/src/main/java/com/antelope/goodbrother/business/main/MainViewHolder.java
+++ app/src/main/java/com/antelope/goodbrother/business/main/MainViewHolder.java
@@ -2,12 +2,10 @@ package com.antelope.goodbrother.business.main;
 
 import android.view.View;
 
-import com.alibaba.android.vlayout.DelegateAdapter;
-import com.alibaba.android.vlayout.VirtualLayoutManager;
 import com.alibaba.android.vlayout.layout.LinearLayoutHelper;
 import com.antelope.goodbrother.R;
-import com.antelope.goodbrother.widget.FootViewAdapter;
 import com.antelope.goodbrother.widget.LoadMoreRecycleScrollListener;
+import com.antelope.goodbrother.widget.LoadMoreRecycleView;
 import com.nirvana.zmkj.base.BaseViewHolder;
 import com.nirvana.zmkj.manager.Page;
 
@@ -18,17 +16,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.RecyclerView;
 
 
 public class MainViewHolder extends BaseViewHolder<MainPresenter> {
-    private RecyclerView recyclerView;
-    private DelegateAdapter delegateAdapter;
+    private LoadMoreRecycleView recyclerView;
     private List<MainEntity> mainEntities;
     private MainAdapter mainAdapter;
-    private FootViewAdapter footViewAdapter;
-    private List<String>footerData;
-    private LoadMoreRecycleScrollListener loadMoreRecycleScrollListener;
 
     public MainViewHolder(AppCompatActivity activity) {
         super(activity);
@@ -41,48 +34,33 @@ public class MainViewHolder extends BaseViewHolder<MainPresenter> {
 
     @Override
     public int getLayoutId() {
-        return R.layout.activity_main;
+        return R.layout.activity_main2;
     }
 
     @Override
     public void initContent(View contentView) {
         initNavigation(R.drawable.white_back, R.color.navigation_bg, "首页", R.color.white);
         recyclerView = contentView.findViewById(R.id.recyclerView);
-        VirtualLayoutManager linearLayoutManager = new VirtualLayoutManager(activity);
-        delegateAdapter = new DelegateAdapter(linearLayoutManager);
-        recyclerView.setLayoutManager(linearLayoutManager);
         mainEntities = new ArrayList<>();
-        mainAdapter = new MainAdapter(activity, new LinearLayoutHelper(0), mainEntities);
-        delegateAdapter.addAdapter(mainAdapter);
-        footerData=new ArrayList<>();
-        footerData.add(FootViewAdapter.LOADING);
-        footViewAdapter=new FootViewAdapter(activity,new LinearLayoutHelper(0),footerData);
-        delegateAdapter.addAdapter(footViewAdapter);
-        recyclerView.setAdapter(delegateAdapter);
+        mainAdapter = new MainAdapter(activity, new LinearLayoutHelper(10), mainEntities);
+        recyclerView.setContentAdapter(mainAdapter);
+        recyclerView.setOnLoadMoreListener(new LoadMoreRecycleScrollListener.OnLoadMoreListener() {
+            @Override
+            public void onLoadMore(int pageIndex) {
+                presenter.getGiftIndex(pageIndex);
+            }
+
+            @Override
+            public void onLoadComplete() {
+
+            }
+        });
         presenter.getGiftIndex(Page.PAGE_INIT);
     }
 
     @Subscribe(threadMode = ThreadMode.MAIN)
     public void result(MainModel mainModel) {
-
-        if (loadMoreRecycleScrollListener == null) {
-            loadMoreRecycleScrollListener = new LoadMoreRecycleScrollListener(new LoadMoreRecycleScrollListener.OnLoadMoreListener() {
-                @Override
-                public void onLoadMore(int pageIndex) {
-                    presenter.getGiftIndex(pageIndex);
-                }
-
-                @Override
-                public void onLoadComplete() {
-                    footerData.clear();
-                    footerData.add(FootViewAdapter.LOAD_COMPLETE);
-                    footViewAdapter.notifyDataSetChanged();
-                }
-            });
-            loadMoreRecycleScrollListener.setTotalPage(mainModel.getTotalPage());
-            recyclerView.addOnScrollListener(loadMoreRecycleScrollListener);
-        }
-
+        recyclerView.setTotalPage(mainModel.getTotalPage());
         mainEntities.addAll(mainModel.getList());
         mainAdapter.notifyDataSetChanged();
     }
